9410c072e8dc1f2fb5c838bfc40b2cbcee1dd845
Crowdbotics <engineering@crowdbotics.com>
2023-06-23 21:02:09 +0000

    Update store.

--- store/signup/signup_response_post_Signups.slice.js
+++ store/signup/signup_response_post_Signups.slice.js
@@ -1,9 +1,9 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { apiService } from "./api"
-export const signup_post_api_v1_signup_read = createAsyncThunk(
-  "signup_response_post_Signups/signup_post_api_v1_signup_read",
+export const signup_post_api_v1_signup_create = createAsyncThunk(
+  "signup_response_post_Signups/signup_post_api_v1_signup_create",
   async payload => {
-    const response = await apiService.signup_post_api_v1_signup_read(payload)
+    const response = await apiService.signup_post_api_v1_signup_create(payload)
     return response.data
   }
 )
@@ -13,18 +13,18 @@ const signup_response_post_SignupsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: {
-    [signup_post_api_v1_signup_read.pending]: (state, action) => {
+    [signup_post_api_v1_signup_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [signup_post_api_v1_signup_read.fulfilled]: (state, action) => {
+    [signup_post_api_v1_signup_create.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities.push(action.payload)
         state.api.loading = "idle"
       }
     },
-    [signup_post_api_v1_signup_read.rejected]: (state, action) => {
+    [signup_post_api_v1_signup_create.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
@@ -33,6 +33,6 @@ const signup_response_post_SignupsSlice = createSlice({
   }
 })
 export default {
-  signup_post_api_v1_signup_read,
+  signup_post_api_v1_signup_create,
   slice: signup_response_post_SignupsSlice
 }
